c6475929c17bd34b0175371a95b717fd7a4c6f77
mafimusic5-beep <mafimusic5@gmail.com>
2026-09-18 00:45:09 +0300

    Point legacy privacy constant to Skryon policy

--- app/src/main/java/com/v2ray/ang/AppConfig.kt
+++ app/src/main/java/com/v2ray/ang/AppConfig.kt
@@ -136,7 +136,7 @@ object AppConfig {
     const val APP_API_URL = "https://api.github.com/repos/2dust/v2rayNG/releases"
     const val APP_ISSUES_URL = "$APP_URL/issues"
     const val APP_WIKI_MODE = "$APP_URL/wiki/Mode"
-    const val APP_PRIVACY_POLICY = "$GITHUB_RAW_URL/2dust/v2rayNG/master/CR.md"
+    const val APP_PRIVACY_POLICY = "https://skryon.ru/privacy.html"
     const val APP_PROMOTION_URL = "aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw="
     const val TG_CHANNEL_URL = "https://t.me/github_2dust"
     const val DELAY_TEST_URL = "https://skryon.ru/health"
